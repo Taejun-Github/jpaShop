c939a2c4043798b79750071a187c25ffe291d945
TAEJUN\ausch <tjkimin2021@gmail.com>
2022-03-11 13:12:01 +0900

    엔티티 설계시 주의점

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,14 +19,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne // 한 멤버가 여러 주문을 가지기 때문에 연관관계의 주인은 주문이다.
+    @ManyToOne(fetch = FetchType.LAZY) // 한 멤버가 여러 주문을 가지기 때문에 연관관계의 주인은 주문이다.
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order") // 한 주문이 여러 아이템을 가지기 때문에 연관관계의 주인은 아이템이다.
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 한 주문이 여러 아이템을 가지기 때문에 연관관계의 주인은 아이템이다.
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,4 +34,24 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [Order, Cancel]
+
+    // 연관관계 메서드
+    public void setMember(Member member) {
+        // 멤버를 세팅할 때 양방향으로 매핑되어 있는 orders 까지 한번에 세팅해준다.
+        // 이것은 다대일이므로 이렇게 세팅
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        // 마찬가지로 양방향으로 매핑되어 있는 item 까지 한번에 세팅해준다.
+        // 이것은 일대다이므로 이렇게 세팅
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -15,11 +15,11 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne // 하나의 상품은 여러 번 주문될 수 있으므로 주문아이템이 다, 아이템이 일 관계이다.
+    @ManyToOne(fetch = FetchType.LAZY) // 하나의 상품은 여러 번 주문될 수 있으므로 주문아이템이 다, 아이템이 일 관계이다.
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne  // 하나의 주문은 여러 주문상품을 가질 수 있으므로 주문아이템이 다, 주문이 일 관계이다.
+    @ManyToOne(fetch = FetchType.LAZY)  // 하나의 주문은 여러 주문상품을 가질 수 있으므로 주문아이템이 다, 주문이 일 관계이다.
     @JoinColumn(name = "order_id")
     private Order order;
 
