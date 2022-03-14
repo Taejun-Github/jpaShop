d99f14e3b1637668e0edab2582ad562175328553
TAEJUN\ausch <tjkimin2021@gmail.com>
2022-03-14 15:25:51 +0900

    주문 검색 기능

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -20,5 +25,74 @@ public class OrderRepository {
     public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//        return em.createQuery("select o from Order o join o.member m" +
+//                        " where o.status= :status "  +
+//                        " and m.name like :name", Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setMaxResults(1000) // 최대 1000개까지만 가능
+//                .getResultList();
+//    } 동적 쿼리를 생성해야 하므로 조건을 만들어줘야 한다.
+
+    /**
+     * JPA Criteria 로 동적 쿼리를 생성한다. 이것은 실무에서 사용하지 않고 QueryDsl을 반드시 써야 한다.
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if(orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        // 회원 이름 검색
+        if(StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
+
+    // QueryDsl로 작성
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//        QOrder order = QOrder.order;
+//        QMember member = QMember.member;
+//
+//        return query
+//                .select(order)
+//                .from(order)
+//                .join(order.member, member)
+//                .where(statusEq(orderSearch.getOrderStatus())).
+//                    nameLike(orderSearch.getMemberName())
+//                .limit(1000)
+//                .fetch();
+//    }
+//
+//    private BooleanExpression statusEq(OrderStatus statusCond) {
+//        if(statusCond == null) {
+//            return null;
+//        }
+//        return order.status.eq(statusCond);
+//    }
+//
+//    private BooleanExpression nameLike(String nameCond) {
+//        if(!StringUtils.hasText(nameCond)) {
+//            return null;
+//        }
+//        return member.name.like(nameCond);
+//    }
 
 }
